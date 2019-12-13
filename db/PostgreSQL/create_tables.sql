b8f8a753c217ad8b924624892c2e1c62f53f2d67
g-otn <g-otn@hotmail.com>
2019-12-13 04:20:34 -0300

    create: initial (empty) create tables sql script

---
+++ db/PostgreSQL/create_tables.sql
@@ -0,0 +1,38 @@
+CREATE TABLE ecommerce.usuarios
+(
+
+);
+ALTER TABLE ecommerce.usuarios
+    OWNER TO eletivaweb_usuario;
+
+
+CREATE TABLE ecommerce.administradores
+(
+
+);
+ALTER TABLE ecommerce.administradores
+    OWNER TO eletivaweb_usuario;
+
+
+CREATE TABLE ecommerce.clientes
+(
+
+);
+ALTER TABLE ecommerce.clientes
+    OWNER TO eletivaweb_usuario;
+
+
+CREATE TABLE ecommerce.produtos
+(
+
+);
+ALTER TABLE ecommerce.produtos
+    OWNER TO eletivaweb_usuario;
+
+
+CREATE TABLE ecommerce.vendas
+(
+
+);
+ALTER TABLE ecommerce.vendas
+    OWNER TO eletivaweb_usuario;
